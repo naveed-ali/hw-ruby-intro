3e0578663590c5a121b64090dfc7dda59f9dbcd6
Naveed Ali <naveedaa@gmail.com>
2015-10-27 00:34:53 +0000

    Completed homework

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,74 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  unless arr.empty?
+    arr.inject { |total, n| total + n }
+  else
+    0
+  end
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  sorted_arr = arr.sort
+  if arr.empty?
+    0
+  elsif arr.length == 1
+    sorted_arr[0]
+  else
+    sorted_arr[-1] + sorted_arr[-2]
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  found = false
+  
+  if arr.length > 1
+    i = 0
+    
+    while !found && i < arr.length
+      j = i + 1
+
+      while !found && j < arr.length
+        if arr[i] + arr[j] == n
+          found = true
+        end
+        
+        j += 1
+      end
+
+      i += 1
+    end
+  end
+  
+  found
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s =~ /^[^\Waeiou]/i
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  s =~ /^[0|1]+$/ and s =~ /00{0,1}$/
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty? or price <= 0
+    @isbn = isbn
+    @price = price
+  end
+  
+  def price_as_string
+    "$#{'%.2f' % @price}"
+  end
 end
